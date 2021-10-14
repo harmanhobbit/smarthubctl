1f9478b804a61643d5ebf5bebf4179075c95aa06
Ian Allaway <66171552+harmanhobbit@users.noreply.github.com>
2021-10-14 23:58:38 +0100

    Add files via upload

---
+++ smarthubctl.py
@@ -0,0 +1,21 @@
+import socket
+import sys
+
+address = "192.168.0.5"
+
+def routeInput(self, input, output):
+  input -= 1
+  output -= 1
+  return "VIDEO OUTPUT ROUTING:\n%s %d\n\n" %(output, input)
+
+BUFFER_SIZE = 2048
+
+# print(routeInput(sys.argv[0], int(sys.argv[1]), int(sys.argv[2])))
+
+smarthubSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+smarthubSocket.connect(("192.168.1." + address, 9990))
+smarthubSocket.send(routeInput(sys.argv[0], int(sys.argv[1]), int(sys.argv[2])))
+data = smarthubSocket.recv(BUFFER_SIZE)
+smarthubSocket.close
+
+#print "Received data:\n\n", data
